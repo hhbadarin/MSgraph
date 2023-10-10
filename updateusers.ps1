48a42894ab8da4d14335818d2afe5d618e8514c7
Haitham Badarin <badarin2050@gmail.com>
2023-10-10 18:00:39 +0300

    Update and rename UpdateUsers.ps1 to updateusers.ps1

---
+++ updateusers.ps1
@@ -0,0 +1,21 @@
+
+#Connect to Graph with the scopes
+Connect-MgGraph -Scopes "User.Read.All","Group.ReadWrite.All"
+
+
+#Create password profile
+$PasswordProfile = @{
+  Password                             = $User.Password
+  ForceChangePasswordNextSignIn        = $false
+  ForceChangePasswordNextSignInWithMfa = $false
+}
+  #Update-MgUser
+  #Specify the path of the CSV file
+  $CSVFilePath = "$Home/Desktop/newusers.csv/Updateusers.csv"
+  #Import data from CSV file
+  $Accounts = Import-Csv -Path $CSVFilePath
+  #Loop through each row containing user details in the CSV file
+  ForEach ($Account in $Accounts) {
+    Update-MgUser -UserId $Account.UserPrincipalName -JobTitle $Account.JobTitle -GivenName $Account.GivenName -Surname $Account.Surname -Department $Account.Department}
+  }
+  
