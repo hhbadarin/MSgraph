34f02f4b707c966f515efc696145384affd5dff4
Haitham Badarin <badarin2050@gmail.com>
2023-11-29 08:02:51 +0200

    Added code to export Classes with 0 members

--- Groups.ps1
+++ Groups.ps1
@@ -11,3 +11,20 @@ ForEach ($group in $SynchedGroups) {
     Remove-MgGroup -GroupId $group.Id
     Write-Host ("Successfully deleted {0}" -f $group.DisplayName) -ForegroundColor Green
 }
+
+
+#Export Classes with 0 members 
+$Groups = Get-MgGroup -All | Where-Object {$_.MailNickName -like '*Section_*'} | Select-Object id, DisplayName, MailNickname
+$Report = [System.Collections.Generic.List[Object]]::new()
+ForEach ($Group in $Groups) {
+    if ((Get-MgGroupMember -Groupid  $group.id).count -eq 0) {
+        $ReportLine = [PSCustomObject]@{ 
+            Group                = $Group.displayName
+            Id                   = $Group.id
+            Mail                 = $Group.mail 
+            Created              = $Group.createdDateTime } 
+            $Report.Add($ReportLine)
+    }
+}
+$Report | Out-GridView
+$Report | Export-CSV -Path "$Home/Desktop/ClassesWithZeroMembers.csv" -Encoding UTF8
